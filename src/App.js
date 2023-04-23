e867eab97020fa8eb8eae083c60804afeb0e9a86
mayank parihar <parihar.mayank28@gmail.com>
2023-04-23 17:06:18 +0530

    my commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SlotM from './Slotmach';
+
+
+const App = () => {
+  return (
+    <>
+      <h1 className="heading_style">
+        {" "}
+        🎰 Welcome to{" "}
+        <span style={{ fontWeight: "bold" }}> Slot machine game 🎰</span>
+      </h1>
+      <div className="Slotmachine" >
+      <SlotM x='😄' y='😄' z='😄'/>
+      <SlotM x='😊' y='😝' z='😊'/>
+      <SlotM x='🍎' y='🍌' z='🍎'/>
+      <SlotM x='👳🏻‍♀️' y='👳🏻‍♀️' z='👳🏻‍♀️'/>
+      </div>
+    </>
+  );
+};
+
+export default App;
